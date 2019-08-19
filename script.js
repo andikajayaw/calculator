1e6a92ffbd71b410363a2de2ac4dc9528227e3a1
andikajayaw <andikajayawiguna@gmail.com>
2019-08-19 20:25:01 +0700

    finishing

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function printOutput(num){
     }
 }
 function getFormattedNumber(num){
+    if(num=="-"){
+        return "";
+    }
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
@@ -26,12 +29,51 @@ function reverseNumberFormat(num){
 var operator = document.getElementsByClassName("operator");
 for (var i = 0; i < operator.length; i++){
     operator[i].addEventListener('click',function(){
-        alert("The operator clicked: " + this.id);
+        if(this.id=="clear"){
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output = reverseNumberFormat(getOutput()).toString();
+            if(output){
+                output = output.substr(0,output.length-1);
+                printOutput(output);
+            }
+        }
+        else{
+            var output = getOutput();
+            var history = getHistory();
+            if(output==""&&history!=""){
+                if(isNaN(history[history.length-1])){
+                    history = history.substr(0,history.length-1);
+                }
+            }
+            if(output!="" || history!=""){
+                //condition?true:false
+                output = output==""?
+                output:reverseNumberFormat(output);
+                history = history+output;
+                if(this.id=="="){
+                    var result = eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else{
+                    history = history + this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
     });
 }
 var number = document.getElementsByClassName("number");
 for (var i = 0; i < number.length; i++){
     number[i].addEventListener('click',function(){
-        alert("The number clicked: " + this.id);
+        var output = reverseNumberFormat(getOutput());
+        if(output!=NaN) {
+            output = output+this.id;
+            printOutput(output);
+        }
     });
 }
